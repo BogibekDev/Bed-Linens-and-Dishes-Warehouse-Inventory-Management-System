9091ac0659be19801c483e3fc3b94ce60b91b97e
bogibekdev <bogibekdev@gmail.com>
2023-05-04 07:34:50 +0500

    finish

---
+++ src/Inventory.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Inventory {
+    private List<Product> productList;
+
+    public Inventory() {
+        productList = new ArrayList<>();
+    }
+
+    public void loadInventory(String filePath) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                int id = Integer.parseInt(parts[0]);
+                String name = parts[1];
+                String category = parts[2];
+                double price = Double.parseDouble(parts[3]);
+                int quantity = Integer.parseInt(parts[4]);
+                Product product = new Product(id, name, category, price, quantity);
+                productList.add(product);
+            }
+        }
+    }
+
+    public ArrayList<Product> searchProduct(String keyword, String category) {
+        ArrayList<Product> results = new ArrayList<>();
+        for (Product product : productList) {
+            if ((product.getName().contains(keyword) || Integer.toString(product.getId()).contains(keyword))
+                    && product.getCategory().equals(category)) {
+                results.add(product);
+            }
+        }
+        return results;
+    }
+
+    public void sortInventory(String sortBy) {
+        switch (sortBy.toLowerCase()) {
+            case "name":
+                productList.sort(Comparator.comparing(Product::getName));
+                break;
+            case "price":
+                productList.sort(Comparator.comparing(Product::getPrice));
+                break;
+            case "quantity":
+                productList.sort(Comparator.comparing(Product::getQuantity));
+                break;
+            default:
+                System.out.println("Invalid sort option.");
+                break;
+        }
+    }
+
+    // Getter method for productList
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,104 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        // Display the application menu and execute the selected command
+        Menu();
+    }
+
+    public static void Menu() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Inventory inventory = new Inventory();
+
+        // Load the inventory data
+        String fileName = "src/resource/products.txt";
+        inventory.loadInventory(fileName);
+
+        // Display the application name and developer information
+        System.out.println("Welcome to Bed Linens and Dishes Warehouse Inventory Management System");
+        System.out.println("Developed by Bogibek Matyaqubov");
+
+        boolean exit = false;
+        while (!exit) {
+            // Display the available commands
+            System.out.println("\nPlease select a command:");
+            System.out.println("1. Search for a product");
+            System.out.println("2. Display all products");
+            System.out.println("3. Exit the application");
+
+            // Read the user's input
+            String input = scanner.nextLine();
+
+            switch (input) {
+                case "1":
+                    // Search for a product
+                    System.out.println("Please enter the keyword:");
+                    String keyword = scanner.nextLine();
+
+                    System.out.println("Please enter the category:");
+                    String category = scanner.nextLine();
+
+                    ArrayList<Product> results = inventory.searchProduct(keyword, category);
+
+                    if (results.isEmpty()) {
+                        System.out.println("No products found.");
+                    } else {
+                        System.out.println("Matching products:");
+                        for (Product product : results) {
+                            System.out.println(product.toString());
+                        }
+                    }
+                    break;
+
+                case "2":
+                    // Display all products
+
+                    System.out.println("Please select the sorting order:");
+                    System.out.println("1. Sort by name");
+                    System.out.println("2. Sort by price");
+                    System.out.println("3. Sort by quantity");
+
+                    // Read the user's input
+                    String orderInput = scanner.nextLine();
+
+                    switch (orderInput) {
+                        case "1":
+                            inventory.sortInventory("name");
+                            break;
+
+                        case "2":
+                            inventory.sortInventory("price");
+                            break;
+
+                        case "3":
+                            inventory.sortInventory("quantity");
+                            break;
+
+                        default:
+                            System.out.println("Invalid sorting order.");
+                            break;
+                    }
+
+                    System.out.println("All products:");
+                    for (Product product : inventory.getProductList()) {
+                        System.out.println(product.toString());
+                    }
+
+                    break;
+
+                case "3":
+                    // Exit the application
+                    exit = true;
+                    System.out.println("Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid command.");
+                    break;
+            }
+        }
+    }
+}
